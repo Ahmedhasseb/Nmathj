fe5a628ac797db849d864040e44a09f2268d923f
Ahmedhasseb <ahmedhasseb310@gmail.com>
2024-04-04 16:36:46 +0200

    updateFile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SideComponent } from './modules/side/side.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './modules/Navbar/sidebar/side-bar/side-bar.component';
+import { NavbarComponent } from './modules/NavBar/navbar/navbar.component';
 import { MainComponent } from './modules/Main/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
+   NavbarComponent,
    SideComponent,
    MainComponent
   ],
